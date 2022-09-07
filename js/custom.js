959b8b8cda3c46a5783b3c83032e5960318a672b
NazarenoRios <nazarenolrios@gmail.com>
2022-09-07 14:57:52 -0300

    first commit

---
+++ js/custom.js
@@ -0,0 +1,10 @@
+VanillaTilt.init(document.querySelector("#card-custom"), {
+    startX: 45,
+    startY: 45,
+    reset: false,
+  });
+  
+  //It also supports NodeList
+  VanillaTilt.init(document.querySelectorAll("#card-custom"));
+
+  
